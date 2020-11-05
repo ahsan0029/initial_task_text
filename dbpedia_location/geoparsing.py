101f29f4c07694e65e7fa92e1eb9fd08d2c2f1a5
ahsan0029 <insan.ahsan@hotmail.com>
2020-11-05 14:38:32 +0100

    added annoted text from dbpedia:location and store in json

--- dbpedia_location/geoparsing.py
+++ dbpedia_location/geoparsing.py
@@ -8,13 +8,23 @@ location_filter = {
     'types': "DBpedia:Location",
     'coreferenceResolution': False
 }
-data_2= pd.read_csv(r"C:\Users\insan\OneDrive\Desktop\task\Data_Preprocessing\tweet_pre.csv") 
+
+data_2= pd.read_csv(r"C:\Users\insan\OneDrive\Desktop\task_initial\Data_Preprocessing\tweet_pre.csv") 
 for index, row in data_2.iterrows():
     a=(row['Text'])
-    annotations = spotlight.annotate('https://api.dbpedia-spotlight.org/en/annotate', a,
-                                     filters=location_filter)
+    print(row['Text'])
+    
+    try:
+        annotations = spotlight.annotate('https://api.dbpedia-spotlight.org/en/annotate', str(a), filters=location_filter)
+    except:
+        print("Error occurred ")
+
+    #print(annotations)
     results.append(annotations)
-print(results)
+
+
+with open(r'C:\Users\insan\OneDrive\Desktop\task_initial\dbpedia_location\dbpedia_location.json', 'w') as json_file:
+    json.dump(results, json_file)
 # jsondata = json.dumps(annotations)
 
 # with open(r"C:\Users\insan\OneDrive\Desktop\initial_task/location.json", "w") as f:
