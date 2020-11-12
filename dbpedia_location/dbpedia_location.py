96af0fdc7775537779e378d4652de9dd2a466325
ahsan0029 <insan.ahsan@hotmail.com>
2020-11-13 00:00:33 +0100

    ner using spacy xx_ent_wiki_sm for multi-language

--- dbpedia_location/dbpedia_location.py
+++ dbpedia_location/dbpedia_location.py
@@ -3,6 +3,7 @@ import json
 import pandas as pd
 b=[]
 results = []
+annotations=[]
 location_filter = {
     'policy': "whitelist",
     'types': "DBpedia:Location",
@@ -12,16 +13,19 @@ location_filter = {
 data_2= pd.read_csv(r"C:\Users\insan\OneDrive\Desktop\task_initial\Data_Preprocessing\tweet_pre.csv") 
 for index, row in data_2.iterrows():
     a=(row['Text'])
-    print(row['Text'])
+    
+    # print(type(row['Text']))
     
     try:
-        annotations = spotlight.annotate('https://api.dbpedia-spotlight.org/en/annotate', str(a), filters=location_filter)
+        annotations = spotlight.annotate('https://api.dbpedia-spotlight.org/en/annotate?text={text}', str(a), filters=location_filter)
+        #annotations.append({'text':row['Text']})
+        print(annotations)
     except:
         print("Error occurred ")
 
     #print(annotations)
     results.append(annotations)
-with open(r'C:\Users\insan\OneDrive\Desktop\task_initial\dbpedia_location\dbpedia_location.json', 'w') as json_file:
-    json.dump(results, json_file)
+# with open(r'C:\Users\insan\OneDrive\Desktop\task_initial\dbpedia_location\dbpedia_location.json', 'w') as json_file:
+#     json.dump(results, json_file)
 
     
--- tokenization/token_spacy.py
+++ tokenization/token_spacy.py
@@ -1,13 +1,14 @@
 import spacy
 import pandas as pd
-# import xx_ent_wiki_sm #Multi-language
-import en_core_web_sm
-en_model = en_core_web_sm.load()#xx_ent_wiki_sm.load()
+import xx_ent_wiki_sm #Multi-language
+# import en_core_web_sm
+en_model = xx_ent_wiki_sm.load()
+# en_core_web_sm.load()
 import csv
 import json
 sample_text=''
 
-nlp =spacy.load ('en_core_web_sm')#('en_core_web_sm')
+nlp =spacy.load ('xx_ent_wiki_sm')#('en_core_web_sm')
 df= pd.read_csv(r"C:\Users\insan\OneDrive\Desktop\task_initial\Tweet_data\raw_tweet.csv") 
 for index, row in df.iterrows():
     sample_text+=(row['Text'])
@@ -28,8 +29,8 @@ def tokenize(text):
 
 json_token=json.dumps(tokenize(sample_text))
 # print(tokenize(sample_text))
-with open(r'C:\Users\insan\OneDrive\Desktop\task_initial\tokenization\tweet-tokenized-data.csv', 'w') as json_file:
-    json.dump(tokenize(sample_text), json_file)
+# with open(r'C:\Users\insan\OneDrive\Desktop\task_initial\tokenization\tweet-tokenized-data.csv', 'w') as json_file:
+#     json.dump(tokenize(sample_text), json_file)
 
 
 #POS Tagging
@@ -45,7 +46,7 @@ text_list, pos_list = map(list, zip(*pos_tag(sample_text)))
 d = {'Text': text_list, 'POS Tag': pos_list}
 df_1 = pd.DataFrame(data=d, columns=['Text','POS Tag'])
 # print(df_1)
-df_1.to_csv(r"C:\Users\insan\OneDrive\Desktop\task_initial\tokenization\tweet-POS-Tag-data.csv",index = None)
+# df_1.to_csv(r"C:\Users\insan\OneDrive\Desktop\task_initial\tokenization\tweet-POS-Tag-data.csv",index = None)
 
 #Chunking
 # Detecting the Noun Phrases: NP from the sample_text extracted above
@@ -63,8 +64,8 @@ json_token=json.dumps(chunking(sample_text))
 print(chunking(sample_text))
 
 
-with open(r'C:\Users\insan\OneDrive\Desktop\task_initial\tokenization\tweet-chunked-data.csv', 'w') as json_file:
-    json.dump(chunking(sample_text), json_file)
+# with open(r'C:\Users\insan\OneDrive\Desktop\task_initial\tokenization\tweet-chunked-data.csv', 'w') as json_file:
+#     json.dump(chunking(sample_text), json_file)
 
 # Named Entity Recognition
 # Now we find Named entities
@@ -79,51 +80,5 @@ df_2 = pd.DataFrame(data=d, columns=['Text','Named Entity'])
 # print(df_2)
 df_2.to_csv(r"C:\Users\insan\OneDrive\Desktop\task_initial\tokenization\tweet-spacy-ner_data.csv",index = None)
 
-
-
-# output_files = []
-
-# #used to create the output files required i.e. for tokenized data, POS tagged data, chunked data and ner data.
-# #for each of the three datasets, we need to do these steps, hence defining a separate reusable method
-# def open_out_files(dataset_filename, nlp_toolkit):
-    
-#     #Tokenization
-#     #creating an output file to write Tokenized tagged data for the input dataset
-#     tokenized_out_file = open(dataset_filename+"-[with-"+nlp_toolkit+"]"+"-tokenized_data.csv","w+")
-#     output_files.append(tokenized_out_file) #adding the opened file in the output_files list, so that we can keep track of files we need to close.
-#     #creating csv writer for Tokenized output file
-#     tokenized_csv = csv.writer(tokenized_out_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-#     tokenized_csv.writerow(['Tweet', 'tokens'])
-    
-#     #POS Tagging
-#     #creating an output file to write POS tagged data for the input dataset
-#     pos_out_file = open(dataset_filename+"-[with-"+nlp_toolkit+"]"+"-pos_tagged_data.csv","w+")
-#     output_files.append(pos_out_file) #adding the opened file in the output_files list, so that we can keep track of files we need to close.
-#     #creating csv writer for POS tagged output file
-#     pos_tag_csv = csv.writer(pos_out_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-#     pos_tag_csv.writerow(['Tweet', 'pos_tagged_text'])
-    
-#     #Chunking
-#     #creating an output file to write chunked data for the input dataset
-#     chunk_out_file = open(dataset_filename+"-[with-"+nlp_toolkit+"]"+"-chunked_data.csv","w+")
-#     output_files.append(chunk_out_file) #adding the opened file in the output_files list, so that we can keep track of files we need to close.
-#     #creating csv writer for output file for chunked data
-#     chunked_csv = csv.writer(chunk_out_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-#     chunked_csv.writerow(['Tweet', 'chunks'])
-    
-#     #NER
-#     #creating an output file to write NER data for the input dataset
-#     ner_out_file = open(dataset_filename+"-[with-"+nlp_toolkit+"]"+"-ner_data.csv","w+")
-#     output_files.append(ner_out_file) #adding the opened file in the output_files list, so that we can keep track of files we need to close.
-#     #creating csv writer for output file for NER
-#     ner_csv = csv.writer(ner_out_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-#     ner_csv.writerow(['Tweet', 'ner'])
-    
-#     return tokenized_csv, pos_tag_csv, chunked_csv, ner_csv
-
-# #defining a separate method for closing all the files we open for creation, hence reducing number of lines
-# def close_out_files():
-#     for output_file in output_files:
-#         output_file.close()
 
 
