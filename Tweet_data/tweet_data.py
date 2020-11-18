0cf05a3ba0bcfb8a5541c0a46cb0f50a36d087a5
MD AHSAN <insan.ahsan@hotmail.com>
2020-11-18 21:56:55 +0100

    removed http from tweet data

--- Tweet_data/tweet_data.py
+++ Tweet_data/tweet_data.py
@@ -1,5 +1,5 @@
 import pandas as pd
-data = pd.read_json(r"C:\Users\insan\OneDrive\Desktop\task_initial\tweet_data\all_data.json")
+data = pd.read_json(r"C:\Users\insan\OneDrive\Desktop\task_initial\initial_task_text\tweet_data\all_data.json")
 data_1 = pd.DataFrame([])
 for key, value in data.items():
     if key == "text" :
@@ -7,7 +7,8 @@ for key, value in data.items():
         #select the tweet data from json 
         data_1 = data_1.append(pd.DataFrame({'Tweet': value}))
 #print(data_1)
+data_1['Tweet'] = data_1['Tweet'].replace(r'http\S+', '', regex=True)
 data_2=data_1.head(50)
 #print(data_2)
 
-data_2.to_csv(r"C:\Users\insan\OneDrive\Desktop\task_initial\Tweet_data\raw_tweet.csv",index = None)
+data_2.to_csv(r"C:\Users\insan\OneDrive\Desktop\task_initial\initial_task_text\Tweet_data\raw_tweet.csv",index = None)
