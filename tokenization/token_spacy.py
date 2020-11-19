f73d24e83a027c3401550b070285a60beb8d0e77
MD AHSAN <insan.ahsan@hotmail.com>
2020-11-19 21:50:40 +0100

    annoated data using tag editior spacy

--- tokenization/token_spacy.py
+++ tokenization/token_spacy.py
@@ -7,22 +7,22 @@ en_model = xx_ent_wiki_sm.load()
 import csv
 import json
 df_2 = pd.DataFrame([])
-a=''
-sample_text=''
+
+sample_text="WTF, die grüne Grenze zwischen Österreich und Deutschland ist 800 km lang. Kein Problem für #Flüchtlinge. #Grenzkontrollen ist keine Lösung!"
 nlp =spacy.load ('xx_ent_wiki_sm')#('en_core_web_sm')
-df= pd.read_csv(r"C:\Users\insan\OneDrive\Desktop\task_initial\Tweet_data\raw_tweet.csv") 
-for index, row in df.iterrows():
-    #print(row['Tweet'])
-    sample_text+=(row['Tweet'])
-    a=row=['Tweet']
+# df= pd.read_csv(r"C:\Users\insan\OneDrive\Desktop\task_initial\Tweet_data\raw_tweet.csv") 
+# for index, row in df.iterrows():
+#     #print(row['Tweet'])
+#     #sample_text+=(row['Tweet'])
 
-    def ner(text):
-        ent_docs = en_model(text)
-        return [(ent.text, ent.label_) for ent in ent_docs.ents]
 
-    text_list, ne_list = map(list, zip(*ner(sample_text)))
-    d = {'Tweet': text_list, 'Named Entity': ne_list}
-   # df_2 = pd.DataFrame(data=d, columns=['Tweet','Named Entity'])
-    df_2 = df_2.append(pd.DataFrame(data=d, columns=['Tweet','Named Entity']))
-    print(df_2)
-    doc=nlp(a)
+def ner(text):
+    ent_docs = en_model(text)
+    return [(ent.text, ent.label_) for ent in ent_docs.ents]
+print(ner(sample_text))
+text_list, ne_list = map(list, zip(*ner(sample_text)))
+d = {'Tweet': text_list, 'Named Entity': ne_list}
+df_2 = pd.DataFrame(data=d, columns=['Tweet','Named Entity'])
+
+print(df_2)
+doc=nlp(sample_text)
